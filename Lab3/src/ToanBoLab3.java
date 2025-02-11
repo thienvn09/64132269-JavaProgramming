21b253f340cd2bc2ff8ab723a129e3e3a3218967
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-02-12 00:10:33 +0700

    ứedwed

--- Lab3/src/ToanBoLab3.java
+++ Lab3/src/ToanBoLab3.java
@@ -1,9 +1,26 @@
-
-public class ToanBoLab3 {
+import java.util.Scanner;
+public class ToanboLab3 {
 
 	public static void main(String[] args) {
 	System.out.print("xin chào");
-
+	
 	}
-
+	public boolean SNT(int n)
+	{
+		Scanner nhap = new Scanner(Systeam.out);
+		System.out.print("mời nhập số cần kiểm tra");
+		n=nhap
+		boolean check = true;
+		if(n<2) return false;
+		int i=2;
+		while(i<n)
+		{
+			if(n%i==0) {
+				check = false;
+				beark;
+			}
+			i++;
+		}
+		return check;
+	}
 }
