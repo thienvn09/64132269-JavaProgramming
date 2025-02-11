68a8a17eba0b4ddfe7c119840f9456f23f4c71d3
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-02-11 18:54:16 +0700

    up bai 4

---
+++ Bai1_Hello/src/Lab4_khoiLapPhuong.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Lab4_khoiLapPhuong {
+
+	public static void main(String[] args) {
+		Scanner nhap = new Scanner(System.in);
+		double a,b; 
+		System.out.print("moi nhap canh cua khoi lap phuong : ");
+		a = nhap.nextDouble();
+		b=Math.pow(a, 3);
+		System.out.printf("The tich : %.1f",b);
+
+	}
+
+}
