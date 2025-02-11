c41b95b413061adcd595bbd4dff47c33c93d231a
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-02-11 18:37:33 +0700

    upp

--- Bai1_Hello/src/src/Lab3_HinhChuNhat.java
+++ Bai1_Hello/src/src/Lab3_HinhChuNhat.java
@@ -5,8 +5,7 @@ public class Lab3_HinhChuNhat {
 
 	public static void main(String[] args) {
 		Scanner nhap = new Scanner(System.in);
-		int Cdai  ,Crong, a;
-		double Cv,S;
+		double Cv,S,Cdai  ,Crong, a;
 		System.out.print("moi nhap chieu chieu dai : ");
 		Cdai = nhap.nextInt();
 		System.out.print("moi nhap chieu rong : ");
@@ -15,17 +14,17 @@ public class Lab3_HinhChuNhat {
 			do{
 				System.out.print(" Chieu dai va chieu rong khong duoc bang nhau ! \n ");
 				System.out.print("moi nhap lai chieu chieu dai : ");
-				Cdai = nhap.nextInt();
+				Cdai = nhap.nextDouble();
 				System.out.print("moi nhap lai chieu rong : ");
-				Crong = nhap.nextInt();
+				Crong = nhap.nextDouble();
 			}while(Cdai == Crong);
 		}
 		Cv = (Cdai + Crong) *2;
 		S = (Cdai * Crong);
 		a=Math.min(Cdai, Crong);
-		System.out.printf("CV : %lf \n ",Cv );
-		System.out.printf("CV : %lf \n ", S );
-		System.out.printf("CV : %d ",a );
+		System.out.printf("CV : %.2f \n ",Cv );
+		System.out.printf("Dien tich : %.2f \n ", S );
+		System.out.printf("canh nho  : %.2f ",a );
 		nhap.close();
 	}
 
