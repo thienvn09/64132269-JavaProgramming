824d2c1f4c6afff84869f5d68081a05001d08888
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-02-11 10:13:52 +0700

    xin chao
    lan dau up

---
+++ LapTrinhJava/Bai1_Hello/src/HelloApp.java
@@ -0,0 +1,10 @@
+
+public class HelloApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("xin chao thay em là sinh vien ");
+		System.out.println("em tên lê hoàng thiện ");
+	}
+
+}
