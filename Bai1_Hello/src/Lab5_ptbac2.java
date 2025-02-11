d2799b5d27e0e98458a997a952d6486eefed943b
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-02-11 20:25:26 +0700

    buon ngu
    qq

---
+++ Bai1_Hello/src/Lab5_ptbac2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Lab5_ptbac2 {
+
+	public static void main(String[] args) {
+		float a,b,c,detal;
+		Scanner nhap = new Scanner(System.in);
+		System.out.print("moi nhap so a : ");
+		a = nhap.nextFloat();
+		System.out.print("moi nhap so b : ");
+		b = nhap.nextFloat();
+		System.out.print("moi nhap so c : ");
+		c = nhap.nextFloat();
+	}
+
+}
