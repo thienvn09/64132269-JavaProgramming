c8fd6fba6a34799783c203684b2d73e2f5898bdb
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-02-16 17:46:56 +0700

    ss

--- Lab4/src/Sanpham.java
+++ Lab4/src/Sanpham.java
@@ -3,5 +3,15 @@ public class Sanpham {
 	String Tensp;
 	double dongia;
 	double giamgia;
-	public void 
+	public Sanpham(String Tensp,double dongia,double giamgia)
+	{
+		this.Tensp=Tensp;
+		this.dongia=dongia;
+		this.giamgia=giamgia;
+	}
+	void MoTaSp()
+	{
+		System.out.println(Tensp + "-" + dongia + "-" + giamgia);
+		
+	}
 }
