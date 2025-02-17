d4781e28f51e8502e857167da5a1f3afc4eb7ca2
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-02-17 07:29:02 +0700

    Update Main.java

--- Lab4/src/Main.java
+++ Lab4/src/Main.java
@@ -3,10 +3,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		Sanpham sp1 = new Sanpham(null,0,0);
+        Sanphan sp2 = new Sanpham(null,0,0);
+    
 		sp1.Nhap();
+        sp2.Nhap();
+        sp2.getThueNhapKhau();
 		sp1.getThueNhapKhau();
 		sp1.MoTaSp();
-
+        sp2.MotaSp();
+     
 	}
 
 }
