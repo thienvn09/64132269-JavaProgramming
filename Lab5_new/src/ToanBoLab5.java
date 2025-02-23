fc16997e2213d4df927be804012f0220dc4b5390
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-02-23 22:15:18 +0700

    update

--- Lab5_new/src/ToanBoLab5.java
+++ Lab5_new/src/ToanBoLab5.java
@@ -4,15 +4,28 @@ public class ToanBoLab5 {
     public static void main(String[] args) throws Exception {
         int so,n;
         Scanner nhap = new Scanner(System.in);
-        ArrayList<Double> a = new ArrayList<>;
+        ArrayList<Double> a = new ArrayList<Double>();
+        System.out.print("mời nhập số lượng phần tử trong mảng : ");
+        n= nhap.nextInt();
+        Nhap(a,n);
+        System.out.print("Xuất ra màng hình : \n");
+        Xuat(a,n);
     }
     public static void Nhap(ArrayList<Double> a,int n)
     {
         Scanner nhap = new Scanner(System.in);
+       
         for(int i=0;i<n;i++)
         {
             System.out.print("Nhap phan tu thu "+(i+1)+": ");
             a.add(nhap.nextDouble());
+        }
+    }
+    public static void Xuat(ArrayList<Double> a,int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            System.out.printf("%.2f ",a.get(i));
         }
     }
 }
